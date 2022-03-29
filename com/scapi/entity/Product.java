8daf914cab55e1782a67759f0d5b78dde42bdcf1
meenavel <91412860+meenavel@users.noreply.github.com>
2022-03-29 15:25:28 +1100

    Update Product.java

--- com/scapi/entity/Product.java
+++ com/scapi/entity/Product.java
@@ -1,35 +1,35 @@
-package com.scapi.entity;
+// package com.scapi.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+// import javax.persistence.*;
+// import javax.validation.constraints.NotBlank;
+// import javax.validation.constraints.NotNull;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-//@Entity
-@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int productId;
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// //@Entity
+// @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+// public class Product {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.AUTO)
+//     private int productId;
 
-    @NotBlank(message = "Product Name is Mandatory")
-    @Column(length = 50, nullable = false)
-    private String productName;
+//     @NotBlank(message = "Product Name is Mandatory")
+//     @Column(length = 50, nullable = false)
+//     private String productName;
 
-    @NotBlank(message = "Product Description is Mandatory")
-    private String productDescription;
+//     @NotBlank(message = "Product Description is Mandatory")
+//     private String productDescription;
 
-    @NotNull(message = "Product Amount is Mandatory")
-    private Double productAmount;
+//     @NotNull(message = "Product Amount is Mandatory")
+//     private Double productAmount;
 
-    private int productSoldCount;
-}
+//     private int productSoldCount;
+// }
